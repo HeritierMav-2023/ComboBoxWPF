1a06954343df9b4f3e7d8f084f51fe83b0b48cf0
Heritier Mavinga <h.mavinga-kinav@hotmail.com>
2025-10-06 22:08:57 +0200

    Ajoutez des fichiers projet.

---
+++ ComboBoxWPF/Produit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxWPF
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public decimal Prix { get; set; }
+        public int IdCategorie { get; set; }
+
+        // Nouvelle propriété affichée dans le DataGrid
+        public string NomCategorie { get; set; }
+    }
+
+}
