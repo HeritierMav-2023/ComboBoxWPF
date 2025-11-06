edb7ac3d3176657a1aebf49f7cd34790267ac692
Heritier Mavinga <h.mavinga-kinav@hotmail.com>
2025-11-06 22:07:11 +0100

    code review

---
+++ GestionExpanderTrue/ExpanderPreventCollapseBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GestionExpanderTrue
+{
+    public static class ExpanderPreventCollapseBehavior
+    {
+        // SelectedValue : on bindera SelectedOption ici (OneWay)
+        public static readonly DependencyProperty SelectedValueProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectedValue",
+                typeof(string),
+                typeof(ExpanderPreventCollapseBehavior),
+                new PropertyMetadata(null));
+
+        public static void SetSelectedValue(DependencyObject d, string value) =>
+            d.SetValue(SelectedValueProperty, value);
+
+        public static string GetSelectedValue(DependencyObject d) =>
+            (string)d.GetValue(SelectedValueProperty);
+
+        // BlockValue : la valeur qui empêche la fermeture (ex: "Aucun")
+        public static readonly DependencyProperty BlockValueProperty =
+            DependencyProperty.RegisterAttached(
+                "BlockValue",
+                typeof(string),
+                typeof(ExpanderPreventCollapseBehavior),
+                new PropertyMetadata(null, OnBlockValueChanged));
+
+        public static void SetBlockValue(DependencyObject d, string value) =>
+            d.SetValue(BlockValueProperty, value);
+
+        public static string GetBlockValue(DependencyObject d) =>
+            (string)d.GetValue(BlockValueProperty);
+
+        private static void OnBlockValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Expander expander)
+            {
+                // Supprime l'ancien handler pour éviter les doubles
+                expander.Collapsed -= Expander_Collapsed;
+                if (e.NewValue != null)
+                {
+                    expander.Collapsed += Expander_Collapsed;
+                }
+            }
+        }
+
+        private static void Expander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            if (sender is Expander expander)
+            {
+                string selected = GetSelectedValue(expander);
+                string block = GetBlockValue(expander);
+
+                // Si la valeur sélectionnée correspond à la valeur "bloquante",
+                // on ré-ouvre immédiatement l'expander.
+                if (!string.IsNullOrEmpty(block) && selected == block)
+                {
+                    // On remets IsExpanded à true (dès que possible)
+                    expander.Dispatcher.BeginInvoke(new System.Action(() =>
+                    {
+                        expander.IsExpanded = true;
+                    }));
+                }
+            }
+        }
+    }
+}
---
+++ GestionExpanderTrue/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExpanderTrue
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _selectedOption;
+        private bool _isExpanded;
+
+        public MainViewModel()
+        {
+            Options = new ObservableCollection<string> { "Aucun", "Autre" };
+            SelectedOption = "Aucun";
+            // On veut que l'expander soit ouvert par défaut pour "Aucun"
+            IsExpanded = true;
+        }
+
+        public ObservableCollection<string> Options { get; }
+
+        public string SelectedOption
+        {
+            get => _selectedOption;
+            set
+            {
+                if (_selectedOption != value)
+                {
+                    _selectedOption = value;
+                    OnPropertyChanged();
+                    // si on change vers "Aucun", forcer l'expander ouvert
+                    if (_selectedOption == "Aucun")
+                        IsExpanded = true;
+                }
+            }
+        }
+
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set
+            {
+                if (_isExpanded != value)
+                {
+                    _isExpanded = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string p = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
+    }
+}
